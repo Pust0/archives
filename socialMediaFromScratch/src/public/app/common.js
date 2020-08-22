2b60ddc9f90277ae8110570799cf456ef230ef2c
Varun <varunbhouria78@gmail.com>
2020-08-22 17:04:01 +0530

    get and show all posts on frontend

--- socialMediaFromScratch/src/public/app/common.js
+++ socialMediaFromScratch/src/public/app/common.js
@@ -2,6 +2,7 @@ $(() => {
   $("#navbar").load("/components/navbar.html", loginIfNeeded); //loginIfNeeded is used as a callback function here
   // which only runs after navbar is done loading
   $("#footer").load("/components/footer.html");
+  $("#content").load("/components/all-posts.html", loadPosts);
 });
 
 function loginIfNeeded() {
@@ -24,4 +25,28 @@ function loginIfNeeded() {
     console.log("resuming session as", currentUser.username);
     $("#nav-username").text(currentUser.username); //this line changes the username in the navbar
   }
+}
+
+function loadPosts() {
+  $.get("/api/posts", (posts) => {
+    for (let p of posts) {
+      $("#posts-container").append(
+        $(`
+        <div class="col-4">
+          <div class="card m-2" style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title">${p.title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+            <p class="card-text">${p.body.substr(0, 200)}
+              <a href="#">..read more</a>
+            </p>
+            <a href="#" class="card-link">Comment</a>
+            <a href="#" class="card-link">Like</a>
+            </div>
+          </div>
+        </div>
+        `)
+      );
+    }
+  });
 }
