7af809bf1ba8f219e21101502073fdeeb19b79ed
Varun <varunbhouria78@gmail.com>
2020-09-05 01:12:30 +0530

    not working, dont care

--- CollaborativeTextEditorUsingIPFS/src/app.js
+++ CollaborativeTextEditorUsingIPFS/src/app.js
@@ -1,1 +1,21 @@
-console.log("Hello WORLD");
+const IPFS = require("ipfs");
+
+const ipfs = new IPFS({
+  repo: repo(),
+  EXPERIMENTAL: {
+    pubsub: true,
+  },
+});
+
+ipfs.once("ready", () =>
+  ipfs.id((err, info) => {
+    if (err) {
+      throw err;
+    }
+    console.log("IPFS node ready with address " + info.id);
+  })
+);
+
+function repo() {
+  return "ipfs/demo/" + Math.random();
+}
